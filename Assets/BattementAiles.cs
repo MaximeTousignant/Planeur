d49b39cf92f73c7cb78ac0c7b2d7e98faab392ba
Maxime Tousignant <maxtou@hotmail.com>
2015-07-23 16:15:33 -0400

    Ajout de la scene et des scripts

---
+++ Assets/BattementAiles.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattementAiles : MonoBehaviour {
+
+	public Transform tracker;
+	public Transform aileDroite;
+	public Transform aileGauche;
+
+	private Vector3 pos0Droite;
+	private Vector3 pos0Gauche;
+	private float rayon;
+
+	void Awake () {
+		Vector3 dist = GetComponent<Transform> ().position - aileDroite.position;
+		rayon = Mathf.Abs (dist.magnitude);
+		pos0Droite = aileDroite.localPosition;
+		pos0Gauche = aileGauche.localPosition;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float theta = Mathf.Clamp( 2.0f * tracker.localRotation.eulerAngles.x, -80.0f, 80.0f ) ;
+		float _sin = Mathf.Sin ( Mathf.PI * theta / 180f );
+		float _cos = Mathf.Cos ( Mathf.PI * theta / 180f );
+
+		aileDroite.localRotation = Quaternion.Euler( 0f, 0f, 90f - theta );
+		aileDroite.localPosition = pos0Droite + new Vector3 ( -rayon * (1f - _cos), - rayon * _sin, 0f );
+
+		aileGauche.localRotation = Quaternion.Euler( 0f, 0f, 90f + theta );
+		aileGauche.localPosition = pos0Gauche + new Vector3 ( rayon * (1f - _cos), - rayon * _sin, 0f );
+	}
+}
---
+++ Assets/BirdController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdController : MonoBehaviour {
+
+	public Transform tracker;
+
+	private float cumulPitch;
+	private float pitch0;
+//	private float decroiss;
+
+//	private const float tau = 1.0f;
+	private const float sensibility = 1.0f;
+
+//	private int n; 
+//	private int pointer;
+
+	// Use this for initialization
+	void Awake () {
+
+//		n = Mathf.RoundToInt (3.0f * tau / Time.fixedDeltaTime);
+//		pointer = 0;
+
+		pitch0 = tracker.localRotation.eulerAngles.x;
+		cumulPitch = pitch0;
+
+	}
+	
+	// Update is called once per frame
+
+	void FixedUpdate () {
+
+		cumulPitch *= 0.99f;
+		cumulPitch += tracker.localRotation.eulerAngles.x * Time.fixedDeltaTime; // A voir
+
+		transform.RotateAround (transform.position, transform.right, sensibility * cumulPitch);
+	
+	}
+}
